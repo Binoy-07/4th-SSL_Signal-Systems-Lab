0baf106d047884f6295a13ca65bd807cdb4e7b70
Binoy-07 <108354873+Binoy-07@users.noreply.github.com>
2023-07-15 21:18:59 +0600

    Add files via upload

---
+++ L8_6_2023.m
@@ -0,0 +1,25 @@
+a=[0 0 0 2; 1 0 4 0; 1 0 2 0; 2 2 0 7]
+b=[7 0 2 2; 0 2 0 1; 0 4 0 1; 2 0 0 0] 
+
+disp('Addition : ')
+  a+b
+disp('Substraction : ')
+a-b
+
+disp('Multiplication : ')
+a*b
+
+disp('Division : ')
+a/b
+
+disp('Transpose A : ')
+a'
+
+disp('Transpose B : ')
+c=transpose(b)
+
+disp('Inverse of A matrix : ')
+d=inv(a)
+
+disp('Determinant of B : ')
+e=det(b)
